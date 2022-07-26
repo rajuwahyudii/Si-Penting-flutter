0f456b4fe9851356cdb9be4f38b8df75ec107cc7
Rwhytm <rajuwahyudi842@gmail.com>
2022-07-26 20:41:13 +0700

    splash and wrapper

--- lib/pages/splash.dart
+++ lib/pages/splash.dart
@@ -1,10 +1,34 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:stunting/pages/wrapper.dart';
 import 'package:stunting/theme/color.dart';
 import 'package:stunting/theme/font.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    splashTimer();
+    super.initState();
+  }
+
+  Future<Timer> splashTimer() async {
+    return Timer(const Duration(seconds: 4), onDone);
+  }
+
+  void onDone() {
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(
+        builder: (context) => const Wrapper(),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,26 +36,24 @@ class SplashScreen extends StatelessWidget {
       backgroundColor: backgroundColor,
       body: SafeArea(
         child: Center(
-          child: Container(
-              child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: <Widget>[
-              Image.asset(
-                'assets/images/splash.png',
-                width: 341.54,
-              ),
-              const SizedBox(
-                height: 30,
-              ),
-              Text(
-                'STUNTING',
-                style: splashTextStyle,
-                textScaleFactor: 1,
-              )
-            ],
-          )),
-        ),
+            child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Image.asset(
+              'assets/images/splash.png',
+              width: 341.54,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            Text(
+              'STUNTING',
+              style: splashTextStyle,
+              textScaleFactor: 1,
+            ),
+          ],
+        )),
       ),
     );
   }
