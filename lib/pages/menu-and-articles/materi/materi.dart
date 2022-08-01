402e0cf6c8f5688073e1dc1e5c4fbc06b383df3e
Rwhytm <rajuwahyudi842@gmail.com>
2022-08-01 19:15:29 +0700

    start materi

---
+++ lib/pages/menu-and-articles/materi/materi.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Materi extends StatefulWidget {
+  const Materi({Key? key}) : super(key: key);
+
+  @override
+  State<Materi> createState() => _MateriState();
+}
+
+class _MateriState extends State<Materi> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
