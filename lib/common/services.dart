7beea5fb452fbd2153bf0228ec1d8dae94f74b29
Rwhytm <rajuwahyudi842@gmail.com>
2022-08-08 12:01:57 +0700

    middleware admin and user

--- lib/common/services.dart
+++ lib/common/services.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: avoid_print
+
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
@@ -16,6 +18,7 @@ class AuthServices {
     } catch (e) {
       print('e');
     }
+    return null;
   }
 
   //Register
@@ -53,6 +56,7 @@ class AuthServices {
   //logout
   static Future<User?> signOut() async {
     await auth.signOut();
+    return null;
   }
 
   // ignore: non_constant_identifier_names
--- lib/pages/menu.dart
+++ lib/pages/menu.dart
@@ -1,5 +1,8 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:loading_animation_widget/loading_animation_widget.dart';
 import 'package:stunting/common/services.dart';
 import 'package:stunting/pages/admin/admin.dart';
 import 'package:stunting/theme/color.dart';
@@ -13,140 +16,168 @@ class Menu extends StatefulWidget {
 
 class _MenuState extends State<Menu> {
   int index = 1;
+  static FirebaseAuth auth = FirebaseAuth.instance;
+  static User user = auth.currentUser!;
+  CollectionReference collectionRef =
+      FirebaseFirestore.instance.collection('user');
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
 
-    return Scaffold(
-      backgroundColor: backgroundColor,
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(
-            Icons.logout_outlined,
-          ),
-          onPressed: () async {
-            AuthServices.signOut();
-          },
-        ),
-        backgroundColor: backgroundColor,
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (context) => const AdminPage(),
+    return FutureBuilder<DocumentSnapshot>(
+      future: collectionRef.doc(user.uid).get(),
+      builder:
+          (BuildContext context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+
+          var role = '${data['role']}';
+          return Scaffold(
+            backgroundColor: backgroundColor,
+            appBar: AppBar(
+              leading: IconButton(
+                icon: const Icon(
+                  Icons.logout_outlined,
                 ),
-              );
-            },
-            icon: const Icon(
-              Icons.admin_panel_settings,
-              size: 30,
+                onPressed: () async {
+                  AuthServices.signOut();
+                },
+              ),
+              backgroundColor: backgroundColor,
+              actions: [
+                role == 'admin'
+                    ? IconButton(
+                        onPressed: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const AdminPage(),
+                            ),
+                          );
+                        },
+                        icon: const Icon(
+                          Icons.admin_panel_settings,
+                          size: 30,
+                          color: Colors.white,
+                        ),
+                      )
+                    : const Text('')
+              ],
+              title: Text(
+                index == 1 ? 'Menu' : 'Artikel',
+                style: GoogleFonts.poppins(
+                  fontSize: 23,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              centerTitle: true,
+            ),
+            body: SizedBox(
+              child: SingleChildScrollView(
+                child: Column(
+                  children: <Widget>[
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                      child: Image.asset(
+                        index == 1
+                            ? 'assets/images/menu.png'
+                            : 'assets/images/artikel.png',
+                        width: 180,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            bottomNavigationBar: Container(
+              color: buttonBlueColor,
+              width: size.width,
+              height: 70,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: <Widget>[
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        index = 1;
+                      });
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          ImageIcon(
+                            const AssetImage(
+                              'assets/images/monitor.png',
+                            ),
+                            color: index == 1 ? Colors.white : Colors.black,
+                            size: 25,
+                          ),
+                          Text(
+                            'Menu',
+                            style: GoogleFonts.poppins(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              color: index == 1 ? Colors.white : Colors.black,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  ),
+                  GestureDetector(
+                    onTap: () {
+                      setState(() {
+                        index = 2;
+                      });
+                    },
+                    child: Container(
+                      padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.center,
+                        children: <Widget>[
+                          ImageIcon(
+                            const AssetImage(
+                              'assets/images/menu-icn-white.png',
+                            ),
+                            color: index == 1 ? Colors.black : Colors.white,
+                            size: 25,
+                          ),
+                          Text(
+                            'Artikel',
+                            style: GoogleFonts.poppins(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              color: index == 1 ? Colors.black : Colors.white,
+                            ),
+                          )
+                        ],
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            ),
+          );
+        }
+        return Container(
+          color: backgroundColor,
+          child: Center(
+            child: LoadingAnimationWidget.waveDots(
               color: Colors.white,
+              size: 50,
             ),
           ),
-        ],
-        title: Text(
-          index == 1 ? 'Menu' : 'Artikel',
-          style: GoogleFonts.poppins(
-            fontSize: 23,
-            fontWeight: FontWeight.w600,
-            color: Colors.white,
-          ),
-        ),
-        centerTitle: true,
-      ),
-      body: SizedBox(
-        child: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              const SizedBox(
-                height: 20,
-              ),
-              Center(
-                child: Image.asset(
-                  index == 1
-                      ? 'assets/images/menu.png'
-                      : 'assets/images/artikel.png',
-                  width: 180,
-                ),
-              ),
-            ],
-          ),
-        ),
-      ),
-      bottomNavigationBar: Container(
-        color: buttonBlueColor,
-        width: size.width,
-        height: 70,
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: <Widget>[
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  index = 1;
-                });
-              },
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    ImageIcon(
-                      const AssetImage(
-                        'assets/images/monitor.png',
-                      ),
-                      color: index == 1 ? Colors.white : Colors.black,
-                      size: 25,
-                    ),
-                    Text(
-                      'Menu',
-                      style: GoogleFonts.poppins(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w400,
-                        color: index == 1 ? Colors.white : Colors.black,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            ),
-            GestureDetector(
-              onTap: () {
-                setState(() {
-                  index = 2;
-                });
-              },
-              child: Container(
-                padding: const EdgeInsets.fromLTRB(15, 10, 15, 10),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: <Widget>[
-                    ImageIcon(
-                      const AssetImage(
-                        'assets/images/menu-icn-white.png',
-                      ),
-                      color: index == 1 ? Colors.black : Colors.white,
-                      size: 25,
-                    ),
-                    Text(
-                      'Artikel',
-                      style: GoogleFonts.poppins(
-                        fontSize: 12,
-                        fontWeight: FontWeight.w400,
-                        color: index == 1 ? Colors.black : Colors.white,
-                      ),
-                    )
-                  ],
-                ),
-              ),
-            )
-          ],
-        ),
-      ),
+        );
+      },
     );
   }
 }
