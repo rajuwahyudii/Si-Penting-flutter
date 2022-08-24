719243ce4f119a2515dd6fa2929b00566021817a
Rwhytm <rajuwahyudi842@gmail.com>
2022-08-24 22:56:35 +0700

    add rumus and data for calculator

--- lib/widgets/calculator.dart
+++ lib/widgets/calculator.dart
@@ -1,5 +1,8 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:stunting/models/zscore.dart';
 import 'package:stunting/theme/color.dart';
 import 'package:stunting/theme/font.dart';
 import 'package:stunting/widgets/button.dart';
@@ -15,14 +18,74 @@ class Calculator extends StatefulWidget {
 
 class _CalculatorState extends State<Calculator> {
   TextEditingController bbController = TextEditingController();
+  TextEditingController tbController = TextEditingController();
   int bulansekarang = int.parse(DateTime.now().month.toString());
   int tahunsekarang = int.parse(DateTime.now().year.toString());
   int? bulanlahir;
   int? tahunlahir;
   int? umurtahun;
   int? umurbulan;
+  double? imt;
   DateTime sekarang = DateTime.now();
   DateTime? date;
+  int? rate;
+  int isMale = 1;
+  check(double imt, int umurtahun, int umurbulan) {
+    for (var i = 1; i <= zScore.length; i++) {
+      if (isMale == i) {
+        for (var i = 5; i < 19; i++) {
+          if (umurtahun == i) {
+            for (var i = 0; i < 12; i++) {
+              if (umurbulan == i) {
+                if (imt >=
+                        zScore[isMale - 1]['imt'][umurtahun - 5]['zscore']
+                            [umurbulan]['-3'] &&
+                    imt <=
+                        zScore[isMale - 1]['imt'][umurtahun - 5]['zscore']
+                            [umurbulan]['-2']) {
+                  setState(() {
+                    rate = 1;
+                  });
+                } else if (imt >
+                        zScore[isMale - 1]['imt'][umurtahun - 5]['zscore']
+                            [umurbulan]['-2'] &&
+                    imt <=
+                        zScore[isMale - 1]['imt'][umurtahun - 5]['zscore']
+                            [umurbulan]['+1']) {
+                  setState(() {
+                    rate = 2;
+                  });
+                } else if (imt >
+                        zScore[isMale - 1]['imt'][umurtahun - 5]['zscore']
+                            [umurbulan]['+1'] &&
+                    imt <=
+                        zScore[isMale - 1]['imt'][umurtahun - 5]['zscore']
+                            [umurbulan]['+2']) {
+                  setState(() {
+                    rate = 3;
+                  });
+                } else if (imt >
+                        zScore[isMale - 1]['imt'][umurtahun - 5]['zscore']
+                            [umurbulan]['+2'] &&
+                    imt <=
+                        zScore[isMale - 1]['imt'][umurtahun - 5]['zscore']
+                            [umurbulan]['+3']) {
+                  setState(() {
+                    rate = 4;
+                  });
+                } else {
+                  setState(() {
+                    rate = 0;
+                  });
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -39,7 +102,7 @@ class _CalculatorState extends State<Calculator> {
         ),
         backgroundColor: backgroundColor,
         title: Text(
-          'Kalkulator Gizi',
+          'Kalkulator Gizi 5-18 tahun',
           style: GoogleFonts.poppins(
             fontSize: 23,
             fontWeight: FontWeight.w600,
@@ -75,8 +138,9 @@ class _CalculatorState extends State<Calculator> {
                   width: size.width * 0.8,
                   height: size.width * 0.15,
                   decoration: BoxDecoration(
-                      color: Colors.white,
-                      borderRadius: BorderRadius.circular(17)),
+                    color: Colors.white,
+                    borderRadius: BorderRadius.circular(17),
+                  ),
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
@@ -116,28 +180,30 @@ class _CalculatorState extends State<Calculator> {
                             lastDate: DateTime(DateTime.now().year + 10),
                           ).then(
                             (value) {
-                              setState(() {
-                                date = value;
-                                bulanlahir = value!.month;
-                                umurbulan = bulanlahir! - bulansekarang;
-                                tahunlahir = value.year;
-                                if (umurbulan! > 0) {
-                                  umurtahun = tahunsekarang -
-                                      int.parse(tahunlahir.toString()) +
-                                      1;
-                                } else if (umurbulan == 0) {
-                                  umurtahun = tahunsekarang -
-                                      int.parse(tahunlahir.toString());
+                              setState(
+                                () {
+                                  date = value;
+                                  bulanlahir = value!.month;
+                                  umurbulan = bulanlahir! - bulansekarang;
+                                  tahunlahir = value.year;
+                                  if (umurbulan! > 0) {
+                                    umurtahun = tahunsekarang -
+                                        int.parse(tahunlahir.toString());
+                                  } else if (umurbulan == 0) {
+                                    umurtahun = tahunsekarang -
+                                        int.parse(tahunlahir.toString());
 
-                                  umurbulan = int.parse(umurbulan.toString());
-                                } else {
-                                  umurtahun = tahunsekarang -
-                                      int.parse(tahunlahir.toString());
+                                    umurbulan = int.parse(umurbulan.toString());
+                                  } else {
+                                    umurtahun = tahunsekarang -
+                                        int.parse(tahunlahir.toString()) -
+                                        1;
 
-                                  umurbulan =
-                                      12 + int.parse(umurbulan.toString());
-                                }
-                              });
+                                    umurbulan =
+                                        12 + int.parse(umurbulan.toString());
+                                  }
+                                },
+                              );
                             },
                           );
                         },
@@ -146,6 +212,72 @@ class _CalculatorState extends State<Calculator> {
                           color: backgroundColor,
                           size: 30,
                         ),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.only(left: 40.0),
+                  child: Text(
+                    'Jenis Kelamin',
+                    style: GoogleFonts.poppins(
+                      fontWeight: FontWeight.w600,
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 30.0),
+                  child: Row(
+                    children: [
+                      Row(
+                        children: [
+                          Radio(
+                            value: 1,
+                            groupValue: isMale,
+                            onChanged: (value) {
+                              setState(() {
+                                isMale = value as int;
+                              });
+                            },
+                          ),
+                          Text(
+                            'Laki-laki',
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontSize: 17,
+                            ),
+                          )
+                        ],
+                      ),
+                      Row(
+                        children: [
+                          Radio(
+                            value: 2,
+                            groupValue: isMale,
+                            onChanged: (value) {
+                              setState(() {
+                                isMale = value as int;
+                              });
+                            },
+                          ),
+                          Text(
+                            'Perempuan',
+                            style: GoogleFonts.poppins(
+                              color: Colors.white,
+                              fontSize: 17,
+                            ),
+                          )
+                        ],
                       ),
                     ],
                   ),
@@ -166,22 +298,42 @@ class _CalculatorState extends State<Calculator> {
             ),
             InputButtonWidget(
               obsuretext: false,
-              controller: bbController,
+              controller: tbController,
               keyboard: TextInputType.number,
               label: "tinggi badan (cm)",
             ),
             CustomButtonWidget(
                 text: 'Hitung',
-                press: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => HasilCal(
-                        umurbulan: umurbulan!,
-                        umurtahun: umurtahun!,
+                press: () async {
+                  try {
+                    setState(() {
+                      imt = double.parse(bbController.text) /
+                          ((double.parse(tbController.text) / 100) *
+                              (double.parse(tbController.text) / 100));
+                    });
+                    await Future.delayed(
+                      const Duration(seconds: 1),
+                    );
+
+                    await check(imt!, umurtahun!, umurbulan!);
+                    await Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) => HasilCal(
+                          rate: rate!,
+                          umurbulan: umurbulan!,
+                          umurtahun: umurtahun!,
+                          imt: imt!,
+                        ),
                       ),
-                    ),
-                  );
+                    );
+                  } catch (e) {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                      const SnackBar(
+                        content: Text(" Input Data Kurang"),
+                      ),
+                    );
+                  }
                 },
                 buttonColor: yellowButtonColor,
                 textColor: Colors.black)
--- lib/widgets/hasilcal.dart
+++ lib/widgets/hasilcal.dart
@@ -3,13 +3,20 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:stunting/theme/color.dart';
+import 'package:stunting/widgets/calculator.dart';
 
 class HasilCal extends StatelessWidget {
   int umurtahun;
   int umurbulan;
-  HasilCal({required this.umurtahun, required this.umurbulan, Key? key})
-      : super(key: key);
-
+  double imt;
+  int rate;
+  HasilCal({
+    required this.rate,
+    required this.imt,
+    required this.umurtahun,
+    required this.umurbulan,
+    Key? key,
+  }) : super(key: key);
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -21,7 +28,12 @@ class HasilCal extends StatelessWidget {
             Icons.arrow_back,
           ),
           onPressed: () {
-            Navigator.pop(context);
+            Navigator.pushReplacement(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const Calculator(),
+              ),
+            );
           },
         ),
         backgroundColor: backgroundColor,
@@ -39,51 +51,49 @@ class HasilCal extends StatelessWidget {
         child: SizedBox(
           width: size.width,
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
               Text(
-                'IMT :',
+                'IMT : ${imt.toStringAsFixed(2)}',
                 style: GoogleFonts.poppins(
-                  fontSize: 25,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w600,
+                  color: Colors.white,
+                ),
+              ),
+              Text(
+                umurbulan == 0
+                    ? 'Umur : $umurtahun tahun'
+                    : 'Umur : $umurtahun tahun $umurbulan bulan',
+                style: GoogleFonts.poppins(
+                  fontSize: 20,
                   fontWeight: FontWeight.w600,
                   color: Colors.white,
                 ),
               ),
               Center(
                 child: Text(
-                  'data',
+                  rate == 1
+                      ? 'Gizi Kurang (Thinness)'
+                      : rate == 2
+                          ? 'Gizi Baik Normal'
+                          : rate == 3
+                              ? 'Gizi Lebih (Overweight)'
+                              : rate == 4
+                                  ? 'Obesitas (Obese)'
+                                  : 'Data Input salah',
                   style: GoogleFonts.poppins(
-                    fontSize: 25,
+                    fontSize: 20,
                     fontWeight: FontWeight.w400,
                     color: Colors.white,
-                  ),
-                ),
-              ),
-              Center(
-                child: Container(
-                  width: size.width * 0.9,
-                  decoration: BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.circular(10),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(15.0),
-                    child: Text(
-                      'IMT : ',
-                      textAlign: TextAlign.justify,
-                      style: GoogleFonts.poppins(
-                        fontSize: 18,
-                        fontWeight: FontWeight.w400,
-                        color: Colors.black,
-                      ),
-                    ),
                   ),
                 ),
               ),
               Text(
                 'Saran',
                 style: GoogleFonts.poppins(
-                  fontSize: 25,
+                  fontSize: 20,
                   fontWeight: FontWeight.w600,
                   color: Colors.white,
                 ),
